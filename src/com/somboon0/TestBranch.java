93b278e3e8ba19b1386a8be879275e18c60f3eea
sombos49 <sombos49@CPAHP1211245.corp.ais900.org>
2017-05-12 17:10:51 +0700

    ..

--- src/com/somboon0/TestBranch.java
+++ src/com/somboon0/TestBranch.java
@@ -3,6 +3,6 @@ package com.somboon0;
 public class TestBranch {
 	public static void main(String[] args) {
 		System.out.println("Hello Branch");
-		System.out.println("Hello Branch 2");
+		System.out.println("Hello Branch 3");
 	}
 }
